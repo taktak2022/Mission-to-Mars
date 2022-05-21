9918ecea2374f8d614396024645a7764e19d8a3a
Takemi <tsoshiro@gmail.com>
2022-05-20 19:41:19 -0700

    first commit.  classwork up to module 10.5.1

---
+++ scraping.py
@@ -0,0 +1,19 @@
+
+
+
+# Use the base url to create an absolute url
+img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+img_url
+
+# ## Mars Facts
+
+df = pd.read_html('https://galaxyfacts-mars.com')[0]
+df.head()
+
+df.columns=['Description', 'Mars', 'Earth']
+df.set_index('Description', inplace=True)
+df
+
+df.to_html()
+
+browser.quit()
